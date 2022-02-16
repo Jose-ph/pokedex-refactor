8bceea92891a25f88d33ff124d1f868fd98e7f8b
Jose <jose_esteban_basualdo@hotmail.com>
2022-02-15 21:50:36 -0300

    fixes code problems  after review

--- src/main.js
+++ src/main.js
@@ -33,45 +33,47 @@ function setModal(info) {
   const $modal = document.querySelector("#exampleModalCenter");
   $modal.innerHTML = `
     
-    <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">${info.name}</h5>
-       <!--  <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button> -->
-      </div>
-      <div class="modal-body bg-primary">
-      
-      <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="${info.sprites.front_default}" class="d-block w-100" alt="${info.name}">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="exampleModalLongTitle">${info.name}</h5>
+      <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
     </div>
-    <div class="carousel-item">
-    <img src="${info.sprites.back_default}" class="d-block w-100" alt="${info.name}">
-    </div>
-   <p>Peso : ${info.weight} Altura: ${info.height} Habilidad: ${info.abilities[0].ability.name} </p> 
+    <div class="modal-body bg-primary">
 
+      <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="${info.sprites.front_default}" class="d-block w-100" alt="${info.name}">
+          </div>
+          <div class="carousel-item">
+            <img src="${info.sprites.back_default}" class="d-block w-100" alt="${info.name}">
+          </div>
+          <p>Peso : ${info.weight} Altura: ${info.height} Habilidad: ${info.abilities[0].ability.name} </p>
+
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev">
+          <span id="test" class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+          data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+
+    </div>
   </div>
-  <button  class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span id="test" class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
 </div>
 
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-       
-      </div>
-    </div>
-  </div>
- 
     
     `;
 }
@@ -99,12 +101,9 @@ function getPokemons(url) {
 }
 
 function setNextAndPreviousUrl(nextUrl, previousUrl, urlInfo) {
+  (nextUrl = urlInfo.next), (previousUrl = urlInfo.previous);
 
-   nextUrl = urlInfo.next,
-  previousUrl = urlInfo.previous
-
-  return [nextUrl,previousUrl]
-  
+  return [nextUrl, previousUrl];
 }
 
 function getPokemonUrl(pokemon) {
@@ -112,17 +111,18 @@ function getPokemonUrl(pokemon) {
 }
 
 function handleQuery(url) {
-  
-  let nextUrl = ""
-   let previousUrl = ""
+  let nextUrl = "";
+  let previousUrl = "";
   getPokemons(url)
     .then((response) => {
+      let changePageUrls = setNextAndPreviousUrl(
+        nextUrl,
+        previousUrl,
+        response
+      );
 
-      
-      let changePageUrls = setNextAndPreviousUrl(nextUrl, previousUrl, response)
-      
-      setNextButton(changePageUrls[0])
-      setPreviousButton(changePageUrls[1])
+      setNextButton(changePageUrls[0]);
+      setPreviousButton(changePageUrls[1]);
 
       let pokemons = response.results;
 
@@ -132,48 +132,42 @@ function handleQuery(url) {
       pokemons.forEach((pokemon) => {
         getPokemonUrl(pokemon).then((pokemonUrl) => {
           createCard(pokemonUrl);
-          $handleDetails()
+          $handleDetails();
         });
       });
     });
 }
 
-function setNextButton (url) {
-
+function setNextButton(url) {
   const $nextButton = document.querySelector("#next-btn");
 
   $nextButton.onclick = () => {
     const nextUrl = url;
     if (nextUrl.length !== 0) {
       deleteCards();
-  
+
       handleQuery(nextUrl);
     } else {
       console.error("Error: aún no hay datos");
     }
   };
-
 }
 
-function setPreviousButton (url) {
-
- 
+function setPreviousButton(url) {
   const $previousButton = document.querySelector("#previous-btn");
   $previousButton.onclick = () => {
-    const previousUrl = url
-  
-    if (previousUrl!== "" && previousUrl !== null) {
+    const previousUrl = url;
+
+    if (previousUrl !== "" && previousUrl !== null) {
       deleteCards();
-  
+
       handleQuery(previousUrl);
     } else {
       console.error("Error: no hay página previa");
     }
   };
-
 }
 
 const firstUrl = "https://pokeapi.co/api/v2/pokemon?limit=10";
 
-  handleQuery(firstUrl);
-
+handleQuery(firstUrl);
